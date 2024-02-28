2de48432b97ccd501e175e31a0f62b6961f5d7a1
agareaditya-8055 <adityaagare8055@gmail.com>
2024-02-28 10:57:22 +0530

    Cart Component has been added

---
+++ Yonoha/src/components/header/navItems/cart/Cart.jsx
@@ -0,0 +1,14 @@
+const Cart = () => {
+  return (
+    <div className="text-center m-4 p-4">
+      <h1 className="text-2xl font-bold">Cart</h1>
+      <div className="w-6/12 m-auto">
+        <button className="m-2 p-2 bg-black hover:bg-slate-700 text-white rounded-lg">
+          Clear Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
