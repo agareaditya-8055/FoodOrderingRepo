dab3dd927685643c6f89e9998e92ff64b886408b
agareaditya-8055 <adityaagare8055@gmail.com>
2024-02-29 00:32:10 +0530

    styling changes has been done

--- Yonoha/src/components/body/restCard/Shimmer.jsx
+++ Yonoha/src/components/body/restCard/Shimmer.jsx
@@ -1,25 +1,69 @@
+import { useSelector } from "react-redux";
+
 const Shimmer = () => {
+  const isDarkMode = useSelector((state) => state.theme.darkMode);
   return (
     <div className="shimmer-container flex flex-wrap">
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
-      <div className="shimmer-card mx-1 my-4 w-[232px] h-[400px] bg-gray-100 rounded-lg"></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
+      <div
+        className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
+          isDarkMode ? "bg-bgCard" : "bg-gray-100"
+        } rounded-lg`}
+      ></div>
     </div>
   );
 };
