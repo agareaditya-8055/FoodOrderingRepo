6269e965aaeb304380d4c252f8ee17325980b5c0
agareaditya-8055 <adityaagare8055@gmail.com>
2024-03-02 23:08:52 +0530

    authslice has been added

--- Yonoha/src/store/appStore.js
+++ Yonoha/src/store/appStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
 import themeReducer from "./slices/themeSlice";
+import authReducer from "./slices/authSlice";
 const appStore = configureStore({
   reducer: {
+    auth: authReducer,
     cart: cartReducer,
     theme: themeReducer,
   },
