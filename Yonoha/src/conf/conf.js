899b0faf83e4d27746dd589a1034d3aa4f0d5151
agareaditya-8055 <adityaagare8055@gmail.com>
2024-03-02 23:14:59 +0530

    e-variable added in conf file

---
+++ Yonoha/src/conf/conf.js
@@ -0,0 +1,7 @@
+const conf = {
+  appwriteUrl: String(process.env.PARCEL_APPWRITE_URL),
+  appwriteProjectId: String(process.env.PARCEL_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(process.env.PARCEL_APPWRITE_DATABASE_ID),
+};
+
+export default conf;
