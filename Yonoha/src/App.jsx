d6d5ac217c0d4b7cb4bade0e98704c6c27431a68
agareaditya-8055 <adityaagare8055@gmail.com>
2024-03-05 16:36:30 +0530

    unwanted code has been removed

--- Yonoha/src/App.jsx
+++ Yonoha/src/App.jsx
@@ -1,33 +1,11 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Header from "./components/header/Header";
 import "./index.css";
 import { Outlet } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import authService from "./appwrite/auth";
-import { login, logout } from "./store/slices/authSlice";
+import { useSelector } from "react-redux";
 
 function App() {
   const isDarkMode = useSelector((state) => state.theme.response);
-
-  const dispatch = useDispatch();
-  const checkLoggedInUser = async () => {
-    try {
-      const userData = await authService.getCurrentUser();
-      console.log("User data:", userData); // Log user data
-      if (userData) {
-        dispatch(login({ userData }));
-      } else {
-        dispatch(logout());
-      }
-    } catch (error) {
-      console.error("Error fetching current user:", error);
-      dispatch(logout());
-    }
-  };
-
-  useEffect(() => {
-    checkLoggedInUser();
-  }, [dispatch]);
 
   useEffect(() => {
     document.querySelector("html").classList.remove("light", "dark");
@@ -36,10 +14,8 @@ function App() {
 
   return (
     <>
-      {/* <UserContext.Provider value={{ loggedInUser: userName, setUserName }}> */}
       <Header />
       <Outlet />
-      {/* </UserContext.Provider> */}
     </>
   );
 }
