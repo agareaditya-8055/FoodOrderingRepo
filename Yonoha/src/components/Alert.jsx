7fdbf5d751989b04c8615ffa9accba60f18e8dff
agareaditya-8055 <adityaagare8055@gmail.com>
2024-03-08 23:20:52 +0530

    custom hook for aler has been added

--- Yonoha/src/components/Alert.jsx
+++ Yonoha/src/components/Alert.jsx
@@ -1,17 +1,8 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { clearAlert } from "../store/slices/alertSlice";
+import React from "react";
+import { useAlert } from "../utils/useAlert";
 
 const Alert = ({ message, type }) => {
-  const dispatch = useDispatch();
-  console.log(type);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      dispatch(clearAlert());
-    }, 3000); // Clear the alert after 3 seconds
-
-    return () => clearTimeout(timer); // Clear the timer if the component is unmounted
-  }, [dispatch]);
+  const alertType = useAlert(type);
 
   // Define the color classes based on the type prop
   const colorClasses = {
@@ -23,12 +14,12 @@ const Alert = ({ message, type }) => {
   // Add the transition and transform classes to enable the sliding animation
   return (
     <div
-      className={`p-3 border-l-4 rounded-md ${colorClasses[type]} transition-all w-1/4 transform duration-500 ease-in-out fixed top-4 right-4`}
+      className={`p-3 border-l-4 rounded-md ${colorClasses[alertType]} transition-all w-1/4 transform duration-500 ease-in-out fixed top-4 right-4`}
       role="alert"
       // Use the data-te-animation attribute to specify the animation name and direction
       data-te-animation="slide-top"
     >
-      <p className="font-bold">{type.toUpperCase()}</p>
+      <p className="font-bold">{alertType.toUpperCase()}</p>
       <p>{message}</p>
     </div>
   );
