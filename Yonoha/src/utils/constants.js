321e04ed03664abb2723686636b6de7c5354de5f
agareaditya-8055 <adityaagare8055@gmail.com>
2024-03-20 20:52:41 +0530

    dummy

--- Yonoha/src/utils/constants.js
+++ Yonoha/src/utils/constants.js
@@ -2,7 +2,7 @@ export const LOGO_URL =
   "https://s3.amazonaws.com/thumbnails.venngage.com/template/6114cd0a-e706-4e1b-85b5-8aca4c480570.png";
 
 export const CDN_URL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 
 export const CARD_API =
   "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1458004&lng=79.0881546&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
