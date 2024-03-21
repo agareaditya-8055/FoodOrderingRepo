6185058d03e8d56caa1d1dd80891517c519e7d79
agareaditya-8055 <adityaagare8055@gmail.com>
2024-03-21 23:46:38 +0530

    Loader Component has been added

--- Yonoha/tailwind.config.js
+++ Yonoha/tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
       "2xl": "1536px",
     },
     extend: {
+      animation: {
+        spin: "spin 2s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
       colors: {
         textColor: "#B3B3B3",
         htmlColor: "#202020",
