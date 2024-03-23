5649976fe9a2ea1cc68b3a97b6ee2e37238a798f
agareaditya-8055 <adityaagare8055@gmail.com>
2024-03-23 23:59:12 +0530

    design has been upgraded

--- Yonoha/src/components/body/restCard/Shimmer.jsx
+++ Yonoha/src/components/body/restCard/Shimmer.jsx
@@ -2,18 +2,30 @@ import { useSelector } from "react-redux";
 
 const Shimmer = () => {
   const isDarkMode = useSelector((state) => state.theme.darkMode);
-  const shimmerCards = new Array(12).fill(0); // Create an array with cardCount elements
+  // You can adjust the number of shimmer items based on your design
+  const shimmerItems = Array.from({ length: 20 }, (_, index) => index + 1);
 
   return (
-    <div className="shimmer-container flex flex-wrap">
-      {shimmerCards.map((_, index) => (
-        <div
-          key={index}
-          className={`shimmer-card mx-1 my-4 w-[232px] h-[400px] ${
-            isDarkMode ? "bg-bgCard" : "bg-gray-100"
-          } rounded-lg`}
-        ></div>
-      ))}
+    <div className="Food-menu max-w-screen-lg mx-auto mt-40">
+      <div className="Food-card flex flex-wrap w-full my-5">
+        {shimmerItems.map((item) => (
+          <div
+            key={item}
+            className={`shimmer-item  ${
+              isDarkMode ? "bg-bgCard" : "bg-gray-100"
+            } rounded-lg `}
+          >
+            <div className="shimmer-img"></div>
+            <div className="shimmer-details p-3">
+              <div className="shimmer-title h-4 w-4/5 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 mb-1"></div>
+              <div className="shimmer-rating h-5 w-10 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 mb-1"></div>
+              <div className="shimmer-category h-4 w-1/2 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 mb-1"></div>
+              <div className="shimmer-cost h-4 w-3/10 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 mb-1"></div>
+              <div className="shimmer-time h-4 w-2/5 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200"></div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
