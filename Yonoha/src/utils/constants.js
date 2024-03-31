067906c944889baa65b0f640d6731f9d462165cc
agareaditya-8055 <adityaagare8055@gmail.com>
2024-03-31 17:11:28 +0530

    url for api has been changed

--- Yonoha/src/utils/constants.js
+++ Yonoha/src/utils/constants.js
@@ -5,7 +5,7 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const CARD_API =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1458004&lng=79.0881546&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "http://localhost:3000/api/restaurants?lat=21.1458004&lng=79.0881546&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_API =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1458004&lng=79.0881546&restaurantId=";
+  "http://localhost:3000/api/menu?page-type=REGULAR_MENU&lat=21.1458004&lng=79.0881546&restaurantId=";
